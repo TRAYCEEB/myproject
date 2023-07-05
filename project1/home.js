705fa049bfded6a5de4797918cc085ec779c256a
TRAYCEEB <133964178+TRAYCEEB@users.noreply.github.com>
2023-07-05 01:39:13 +0100

    Add files via upload

---
+++ project1/home.js
@@ -0,0 +1,13 @@
+var salute=document.getElementById('but2')
+
+salute.addEventListener('click', function(){
+    salute.style. backgroundColor='white';
+    salute.style.fontWeight='200px';
+    salute.style.textTransform='uppercase';
+    salute.style.height='80px';
+    salute.style.width='250px';
+    salute.style.marginTop='50px'
+    salute.style.marginright='100px'
+    
+  
+})
